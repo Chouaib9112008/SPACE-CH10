256340bf09f96a21f8e27cc13a8b289304fd7b1e
Chouaib9112008 <chouaibessolahi233@gmail.com>
2024-05-30 23:02:24 +0200

    SPACE CH10

---
+++ PASSWORD GENERATOR/script.js
@@ -0,0 +1,20 @@
+const passwordbox = document.getElementById("password");
+const lenght = 12;
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmonpqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%*&~(){}[]></=";
+const allchars = uppercase + lowercase + number + symbol;
+function createpassword(){
+    let password = "";
+    password += uppercase[Math.floor(Math.random() * uppercase.length)];
+    password += lowercase[Math.floor(Math.random() * lowercase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(lenght > password.length){
+        password += allchars[Math.floor(Math.random() * allchars.length)];
+
+    }
+    passwordbox.value = password;
+}
